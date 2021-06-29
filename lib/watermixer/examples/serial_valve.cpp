cdc388211b55c9fe17331695584a8203b916f8e3
guttih <gudjonholm@gmail.com>
2021-06-29 15:02:34 +0000

    adding serial_valve.cpp example

--- lib/watermixer/examples/serial_valve.cpp
+++ lib/watermixer/examples/serial_valve.cpp
@@ -1,30 +1,25 @@
 /*
- Gets a number (0 - 1) from serial and writes it with PWM to pins 32 and 33.
+ Gets a number (0 - 1) from serial and writes it with PWM to pins 32.
 */
 
 #include <Arduino.h>
 #include "valve.h"
 
 const int pinHot = 32;
-const int pinCold = 33;
 #define FULL_DUTY 255
 #define MAX_DUTY 190
 #define CHANNEL_1 1
-#define CHANNEL_2 2
-
 
 Valve hot (pinHot, CHANNEL_1, FULL_DUTY, MAX_DUTY, 0.0, "Hot valve");
-Valve cold(pinCold,CHANNEL_2, FULL_DUTY, MAX_DUTY, 0.0, "Cold valve");
 
 void analogWrite(double flow) {
+    
     hot.setFlow(flow);
-    cold.setFlow(flow);
 }
 void setup() {
     Serial.begin(115200);
-    
     hot.serialPrintInfo();
-    cold.serialPrintInfo();
+
     Serial.println("Type in a real number from 0 to 1.  For example 0.23");
 }
 
