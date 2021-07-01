a3934f5a750c333e9c4ce8207e7da970400e65cd
guttih <gudjonholm@gmail.com>
2021-07-01 00:03:48 +0000

    adding header to tests

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -3,16 +3,47 @@
 #include "watermixer_test.h"
 #include <unity.h>
 
+
+String repeat(String s, unsigned int count) {
+    String str ="";
+    for(unsigned int ui = 0; ui < count; ui++) {
+        str.concat(s);
+    }
+    return str;
+}
+String concat(String str1, String str2, String str3) {
+    String str = str1;
+    str.concat(str2);
+    str.concat(str3);
+    return str;
+}
+void header(const char *strHeader) {
+    String text = String(" - ") + String(strHeader) + String(" - ");
+    int textLen = text.length();
+    //frame length default is 50
+    const int len = textLen > 46? (textLen+4) : 50;
+    const int spaces = (len - textLen)-2;
+
+    Serial.println(concat("┌", repeat("─", len-2), "┐"));
+    Serial.println(concat("│", repeat(" ", len-2), "│"));
+    int spaceLeft = spaces/2;
+    int spaceRight = spaces - spaceLeft;    
+    String frameLeft = concat("|", repeat(" ", spaceLeft), text);
+    Serial.println(concat(frameLeft, repeat(" ", spaceRight), "|"));
+    Serial.println(concat("│", repeat(" ", len-2), "│"));
+    Serial.println(concat("└", repeat("─", len-2), "┘"));
+}
+
 void testValve(){
-    Serial.println("------ Testing Valve -------");
+    header("Testing Valve");
     RUN_TEST(test_ValveConstructor);
     RUN_TEST(test_ValveSetFlow); 
     RUN_TEST(test_ValveWithLimitSetFlowAndGetPWM); 
     RUN_TEST(test_ValveGetPWM);
 }
-
 void testWaterMixer(){
-    Serial.println("---- Testing WaterMixer ----");
+    
+    header("Testing WaterMixer");
     RUN_TEST(test_WaterMixerConstructor);
     RUN_TEST(test_WaterMixerConstructorWithValues);
     RUN_TEST(test_waterMixerChangeValues);
