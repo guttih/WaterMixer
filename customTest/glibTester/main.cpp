ff6239202c3e3975c3f5b08432544f5e99a86424
guttih <gudjonholm@gmail.com>
2023-05-28 21:12:52 +0000

    adding nowMs to main of glibTester

--- customTest/glibTester/main.cpp
+++ customTest/glibTester/main.cpp
@@ -1,9 +1,21 @@
+// #include <windows.h> 
+// #include<unistd.h>  
+#include <chrono>
 #include <iostream>
 #include "PidController.h"
 
+unsigned long  nowMs() {
+    auto sysTime = std::chrono::system_clock::now();
+    return (unsigned long)std::chrono::duration_cast<std::chrono::milliseconds>(sysTime.time_since_epoch()).count();
+}
+
 int main() {
     PidController pidController;  // Declare a new instance of PidController
-    std::cout << "Hello, World3!" << std::endl;  // Print "Hello, World!" to the console
+    unsigned long x = nowMs();
+    std::cout << "millis" << x << std::endl;  // Print "Hello, World!" to the console
+    while (nowMs() < x + 1000) { continue;  }
+    std::cout << "millis" << nowMs() << std::endl;  // Print "Hello, World!" to the console
+    while (nowMs() < x + 3000) { continue;  }
     // // Call getSuggestedValue() and print the result
     std::cout << "Suggested val ue: " << pidController.getSuggestedValue() << std::endl;
 
